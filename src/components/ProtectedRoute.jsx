7fb51767e026de9d02af81515325da62d294e5ea
InfantFranklin <infant.franklin1@gmail.com>
2024-09-22 20:32:22 +0530

    Fixed page refresh issue on reloading protected route pages.

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,7 +2,11 @@ import { useAuth } from "../context/AuthContext";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   if (!user) {
     return <Navigate to="/login" />;
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
 import axios from "axios";
 
 // Create a context
@@ -7,32 +7,53 @@ const AuthContext = createContext();
 // AuthProvider component that will wrap the app and provide the context
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // Load initial auth state from localStorage on first render
+  useEffect(() => {
+    const storedUser = JSON.parse(localStorage.getItem("userData"));
+    const storedToken = localStorage.getItem("token");
+
+    if (storedUser && storedToken) {
+      setUser(storedUser); 
+    }
+
+    setLoading(false);
+  }, []);
 
   const login = async (userData) => {
-    const userDetails = await axios.get(
-      `${import.meta.env.VITE_PUBLIC_API_URL}/users`,
-      {
-        headers: {
-          Authorization: `Bearer ${userData.token}`,
-        },
-      }
-    );
+    try {
+      const userDetails = await axios.get(
+        `${import.meta.env.VITE_PUBLIC_API_URL}/users`,
+        {
+          headers: {
+            Authorization: `Bearer ${userData.token}`,
+          },
+        }
+      );
 
-    let data = userDetails.data[0];
-    localStorage.setItem("token", JSON.stringify(userData.token));
-    localStorage.setItem("userData", JSON.stringify(data));
-    setUser(data);
+      let data = userDetails.data[0];
+      localStorage.setItem("token", JSON.stringify(userData.token));
+      localStorage.setItem("userData", JSON.stringify(data));
+      localStorage.setItem("isLoggedIn", "true");
+
+      setUser(data);
+    } catch (error) {
+      console.error("Error during login", error);
+    }
   };
 
   const logout = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("userData");
+    localStorage.removeItem("isLoggedIn");
     setUser(null);
   };
 
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
-      {children}
+    <AuthContext.Provider value={{ user, login, logout, loading }}>
+      {!loading && children}{" "}
+      {/* Render children only after checking auth state */}
     </AuthContext.Provider>
   );
 };
